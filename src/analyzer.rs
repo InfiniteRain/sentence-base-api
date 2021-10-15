8e5d3756391803ddcef61f9913e5235fff897f90
InfiniteRain <lysenkodavid@gmail.com>
2021-10-15 15:23:40 +0300

    Refactor mecab analyzer

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -15,24 +15,15 @@ pub struct AnalysisResult {
 }
 
 pub fn analyze_sentence(sentence: &str) -> Vec<Morpheme> {
-    let mut tagger = Tagger::new("");
-    let mut morphemes: Vec<Morpheme> = vec![];
-
-    for node in tagger.parse_to_node(sentence).iter_next() {
-        let node_stat = node.stat as i32;
-        if node_stat == mecab::MECAB_BOS_NODE || node_stat == mecab::MECAB_EOS_NODE {
-            continue;
-        }
-
-        let features: Vec<&str> = node.feature.split(',').collect();
-        let morpheme = &(node.surface)[..(node.length as usize)];
-        let dictionary_form = features[6];
-
-        morphemes.push(Morpheme {
-            morpheme: morpheme.to_string(),
-            dictionary_form: dictionary_form.to_string(),
-        });
-    }
-
-    morphemes
+    Tagger::new("")
+        .parse_to_node(sentence)
+        .iter_next()
+        .filter(|node| {
+            node.stat as i32 != mecab::MECAB_BOS_NODE && node.stat as i32 != mecab::MECAB_EOS_NODE
+        })
+        .map(|node| Morpheme {
+            morpheme: (&(node.surface)[..(node.length as usize)]).to_string(),
+            dictionary_form: node.feature.split(',').collect::<Vec<&str>>()[6].to_string(),
+        })
+        .collect()
 }
