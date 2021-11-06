a0ac37f7da089f12baa298631221aa8851a35051
InfiniteRain <lysenkodavid@gmail.com>
2021-11-06 19:39:11 +0200

    Fix analyzer

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -18,12 +18,19 @@ pub fn analyze_sentence(sentence: &str) -> Vec<Morpheme> {
             node.stat as i32 != mecab::MECAB_BOS_NODE && node.stat as i32 != mecab::MECAB_EOS_NODE
         })
         .map(|node| {
+            let morpheme = (&(node.surface)[..(node.length as usize)]);
             let features = node.feature.split(',').collect::<Vec<&str>>();
+            let dictionary_form = features
+                .get(features.len().wrapping_sub(3))
+                .unwrap_or(&morpheme);
+            let reading = features
+                .get(features.len().wrapping_sub(2))
+                .unwrap_or(&morpheme);
 
             Morpheme {
-                morpheme: (&(node.surface)[..(node.length as usize)]).to_string(),
-                dictionary_form: features[6].to_string(),
-                reading: features[7].to_string(),
+                morpheme: morpheme.to_string(),
+                dictionary_form: dictionary_form.to_string(),
+                reading: reading.to_string(),
             }
         })
         .collect()
